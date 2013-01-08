bb0e33c8416672bcbef0d7ab7a405d3199f7b72c
Octavian Petre <octavsly@gmail.com>
2013-01-08 21:10:57 +0100

    Replace scan to non-scan. Identification shift-regs
    The shift registers in the scan segments are replaced back to no-scan
    elements.
    
    Shift register identification is done in logic_only to prevent that
    test-mode values are set case analysis detects more than necessary.

--- connect_scan_chains.tcl
+++ connect_scan_chains.tcl
@@ -1,3 +1,11 @@
+# Replace scan-ff with normal FF in the shift registers
+replace_scan -to_non_scan
+
+#Insert the observability logic for the clock-gating instances
+clock_gating insert_obs -hierarchical -max_cg 32
+#Run the DFT rule checker, to determine the DFT status of the observability logic:
+check_dft_rules
+
 # make all test clocks compatible
 set_compatible_test_clocks -all
 
