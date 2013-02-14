5f76a10d29550aaa8233ba4ad8798e5d56fbd34a
Octavian Petre <octavsly@gmail.com>
2013-02-14 10:45:57 +0100

    Rename flip-flop pin names during elaborate

--- rc_attributes.tcl
+++ rc_attributes.tcl
@@ -8,4 +8,7 @@ shell mkdir -p /home/scratch/$env(USER)
 set_attribute super_thread_cache			/home/scratch/$env(USER)
 set_attribute super_thread_servers 			{localhost localhost localhost localhost}
 
+# Search on support.cadence.com "RC can't find a pin specified in SDC file" or just "hdl_rename_cdn_flop_pins"
+# TCB procedure will return Q instead of q
+set hdl_rename_cdn_flop_pins {{q Q} {d D} {clk CP}}
 set_attribute truncate false [find / -message TUI-6]
